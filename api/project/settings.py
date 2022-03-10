83579db87f0f1859c1efed9ddbc103b6e83c2258
Edmundo Andrade <edmundo.andrade@meganucleo.mx>
2022-03-09 23:47:50 -0600

    new settings

--- api/project/settings.py
+++ api/project/settings.py
@@ -170,3 +170,9 @@ LOCALE_PATHS = [
     os.path.join(BASE_DIR, 'locale'),
 ]
 
+if DEBUG == False:
+    USE_X_FORWARDED_HOST = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
+
